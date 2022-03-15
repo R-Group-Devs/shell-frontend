6e6c85da57a6e97169d4aea037bd2846cd7ac5ea
Brandon Valosek <bvalosek@gmail.com>
2022-03-15 09:16:57 -0500

    homepage cleanup

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,13 @@
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 import { Content } from '../components/Content';
 import { PageSection } from '../components/PageSection';
 import { Shell } from '../components/Shell';
 
-import { Bangers } from '../components/Bangers';
 import { Whomst } from '../components/Whomst';
 import { Button } from '../components/Button';
-import { getTaps } from '../lib/storage';
 import { TwoPanel } from '../components/TwoPanel';
 
 export const HomePage: FunctionComponent = () => {
-  const [taps, setTaps] = useState(getTaps());
-
-  useEffect(() => {
-    // lol
-    const h = setInterval(() => setTaps(getTaps()), 250);
-    return () => clearInterval(h);
-  }, []);
-
   return (
     <PageSection>
       <Content>
@@ -38,55 +28,43 @@ export const HomePage: FunctionComponent = () => {
             </p>
           </Content>
         </TwoPanel>
-
-        {taps < 0 ? (
-          <>
+        <TwoPanel gapOnCollapse>
+          <Content>
             <p>
-              Experimental public NFT infrastructure
-              <br />
-              from 🦖 <a href="http://playgrounds.wtf/">Playgrounds</a>.
+              Launch interoperable, open NFT projects that{' '}
+              <a href="https://docs.heyshell.xyz/forkanomics" target="_blank">
+                all holders can iterate on
+              </a>
             </p>
             <p>
-              What excites you about <Shell /> ?
+              Deploy portable minting mechanics or reusable NFT rendering logic via{' '}
+              <a href="https://docs.heyshell.xyz/engines" target="_blank">
+                engines
+              </a>
             </p>
-            <Bangers />
-          </>
-        ) : (
-          <>
-            <TwoPanel gapOnCollapse>
-              <Content>
-                <p>Launch interoperable, open NFT projects that anybody can iterate on permissionlessly</p>
-                <p>
-                  Ship custom business logic in a public application market of NFT <strong>engines</strong>{' '}
-                </p>
-                <p style={{ textAlign: 'center' }}>Fork anything</p>
-                {/* <p></p>
-                <p>Deploy a creator-owned collection as part of a digitally-cordinated community.</p>
-                <p>Explore emergent application layers.</p> */}
-                {/* <p>100% trustless and free forever.</p> */}
-              </Content>
-              <Content>
-                <p style={{ textAlign: 'center', marginTop: '22px' }}>
-                  <Shell /> is an open product framework for NFTs
-                </p>
-                <p style={{ textAlign: 'center', marginBottom: '22px' }}>
-                  <Button navTo="/launch">
-                    Launch a <Shell /> collection
-                  </Button>
-                </p>
-              </Content>
-            </TwoPanel>
-            <PageSection>
-              <Content>
-                {/* <p style={{ textAlign: 'center', margin: '60px 0', fontStyle: 'italic' }}>
-                  Experimental public NFT infrastructure
-                  <br />
-                  from 🦖 <a href="http://playgrounds.wtf/">Playgrounds</a>.
-                </p> */}
-              </Content>
-            </PageSection>
-          </>
-        )}
+            <p>
+              Rapidly ship NFT apps that use framework-managed{' '}
+              <a href="https://docs.heyshell.xyz/storage" target="_blank">
+                storage
+              </a>{' '}
+              and shell{' '}
+              <a href="https://docs.heyshell.xyz/deployments-and-links" target="_blank">
+                subgraph
+              </a>
+            </p>
+            <p style={{ textAlign: 'center' }}>Fork anything</p>
+          </Content>
+          <Content>
+            <p style={{ textAlign: 'center', marginTop: '22px' }}>
+              <Shell /> is an open product framework for NFTs
+            </p>
+            <p style={{ textAlign: 'center', marginBottom: '22px' }}>
+              <Button navTo="/launch">
+                Launch a <Shell /> collection
+              </Button>
+            </p>
+          </Content>
+        </TwoPanel>
       </Content>
     </PageSection>
   );
